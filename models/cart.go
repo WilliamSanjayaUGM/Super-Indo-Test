1211b042683756fe7e0b573dccfccd2c4aff2e4e
WilliamSanjayaUGM <65654365+WilliamSanjayaUGM@users.noreply.github.com>
2025-02-24 01:49:08 +0700

    commit changes

--- models/cart.go
+++ models/cart.go
@@ -6,29 +6,39 @@ import (
 
 type Cart struct {
 	gorm.Model
-	UserId     string    `json:"userId"`
-	TotalPrice int       `json:"totalPrice"`
-	TotalItem  int       `json:"totalItem"`
-	ListItem   []Product `json:"listProduct"`
+	UserId      string        `json:"userId"`
+	TotalPrice  int           `json:"totalPrice"`
+	TotalItem   int           `json:"totalItem"`
+	ListProduct []ProductCart `json:"listProduct"`
+}
+
+type ProductCart struct {
+	gorm.Model
+	ProductId string `json:"productId"`
+	Price     int    `json:"price"`
+	UserId    string `json:"userId"`
 }
 
 func AddToCart(userId string, productId string) *Cart {
 	var userCart Cart
 	var product Product
+	var productCart ProductCart
 	DB.Where("user_id = ?", userId).First(&userCart)
 	DB.Where("id = ?", productId).First(&product)
 
-	DB.Where("user_id = ?", userId).First(&userCart)
+	productCart.UserId = userId
+	productCart.ProductId = product.Id
+	productCart.Price = product.Price
 
-	if len(userCart.ListItem) == 0 {
+	if len(userCart.ListProduct) == 0 {
 		userCart.UserId = userId
 		userCart.TotalPrice = product.Price
 		userCart.TotalItem = 1
-		userCart.ListItem = append(userCart.ListItem, product)
+		userCart.ListProduct = append(userCart.ListProduct, productCart)
 	} else {
 		userCart.TotalPrice = userCart.TotalPrice + product.Price
 		userCart.TotalItem = userCart.TotalItem + 1
-		userCart.ListItem = append(userCart.ListItem, product)
+		userCart.ListProduct = append(userCart.ListProduct, productCart)
 	}
 	if DB.Model(&userCart).Where("user_id = ?", userId).Updates(&userCart).RowsAffected == 0 {
 		DB.Create(&userCart)
@@ -36,6 +46,22 @@ func AddToCart(userId string, productId string) *Cart {
 	return &userCart
 }
 
-// func DeletCartByProductId(userId string, productId string) *Cart{
+func DeletCartByProductId(userId string, productId string) *Cart {
+	var userCart Cart
+	var productCart ProductCart
+	var singleProductCart ProductCart
+	DB.Where("user_id = ?", userId).First(&userCart)
+	DB.Where("user_id = ?", userId).Where("product_id", productId).First(&singleProductCart)
+	productPrice := singleProductCart.Price
 
-// }
+	if len(userCart.ListProduct) == 1 {
+		DB.Where("user_id = ?", userId).Delete(&userCart)
+	} else {
+		DB.Where("user_id = ?", userId).Where("product_id", productId).Limit(1).Delete(&productCart)
+		userCart.TotalPrice = userCart.TotalPrice - productPrice
+		userCart.TotalItem = userCart.TotalItem - 1
+		userCart.ListProduct = append(userCart.ListProduct, productCart)
+		DB.Where("user_id = ?", userId).Updates(&userCart)
+	}
+	return &userCart
+}
